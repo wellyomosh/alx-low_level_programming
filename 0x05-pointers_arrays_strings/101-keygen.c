dc7193a0b736e605db7bdfb26d8fdaacc9282c90
wellyomosh <wellyomosh@gmail.com>
2022-04-20 06:21:05 -0700

    My pointers commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - prints valid passwords for the program 101-crackme
+ *Return: 0.
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+	return (0);
+}
