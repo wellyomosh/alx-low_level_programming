22dadb5a58e973d522f2778a7441b2e3cb157df6
wellyomosh <wellyomosh@gmail.com>
2022-05-04 05:06:18 -0700

    My static commit

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * is_numerical - check if it is a digit
+ * @n: Number
+ * Return: If is a number, return 1 else return 0
+ */
+int is_numerical(unsigned int n)
+{
+	return (n >= '0' &&  n <= '9');
+}
+
+/**
+ * _atoi - convert a string to an integer
+ *@s: String
+ * Return: Return the num
+ */
+
+int _atoi(char *s)
+{
+	unsigned int num, i;
+	int sign;
+
+	sign = 1;
+	num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_numerical(s[i]))
+		{
+			num = (s[i] - 48) + num * 10;
+
+			if (s[i + 1] == ' ')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+
+	}
+
+	return (num * sign);
+}
