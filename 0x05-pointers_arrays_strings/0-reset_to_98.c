ad976dbd41a1d109162e078d0127217195e389af
wellyomosh <wellyomosh@gmail.com>
2022-05-10 06:25:08 -0700

    "My more commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * reset_to_98 - Updates the value of n
- *
- * @n: A pointer of the int to be updated
- *
- * Return: void
- */
+* reset_to_98 - updatesa the value of n
+*
+* @n: A pointer of the int to be updated
+*
+* return: void 
+*/
 
 void reset_to_98(int *n)
 {
-	*n = 98;
-}
+*n = 98;
